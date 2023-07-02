1bccec721455628d3496817c1941cea3ebe00639
Anistan Yoganath <93093798+anistanyoganath@users.noreply.github.com>
2023-07-02 16:03:26 +0530

    Add JWT authorization

--- config/app.ts
+++ config/app.ts
@@ -2,12 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import connectDatabase from "./db";
 import ProductController from "../controllers/ProductController";
+import { Authorize } from "./authorize";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", ProductController);
+app.use("/", Authorize, ProductController);
 connectDatabase();
 
 export default app;
